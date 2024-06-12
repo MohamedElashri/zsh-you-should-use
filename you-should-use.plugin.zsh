e9eaa4f892eb6b795b23a2c9559fbbec08ec9bc8
Mohamed Elashri <mail@elashri.com>
2024-06-12 02:54:15 +0000

    optimize printf and handling of ANSI Color Codes

--- you-should-use.plugin.zsh
+++ you-should-use.plugin.zsh
@@ -3,8 +3,12 @@
 export YSU_VERSION='1.7.4'
 
 if ! type "tput" > /dev/null; then
-    printf "WARNING: tput command not found on your PATH.\n"
-    printf "zsh-you-should-use will fallback to uncoloured messages\n"
+    printf "WARNING: tput command not found on your PATH.\nzsh-you-should-use will fallback to uncoloured messages\n"
+    NONE=""
+    BOLD=""
+    RED=""
+    YELLOW=""
+    PURPLE=""
 else
     NONE="$(tput sgr0)"
     BOLD="$(tput bold)"
@@ -14,9 +18,6 @@ else
 fi
 
 function check_alias_usage() {
-    # Optional parameter that limits how far back history is checked
-    # I've chosen a large default value instead of bypassing tail because it's simpler
-    # TODO: this should probably be cleaned up
     local limit="${1:-9000000000000000}"
     local key
 
@@ -24,10 +25,6 @@ function check_alias_usage() {
     for key in "${(@k)aliases}"; do
         usage[$key]=0
     done
-
-    # TODO:
-    # Handle and (&&) + (&)
-    # others? watch, time etc...
 
     local current=0
     local total=$(wc -l < "$HISTFILE")
@@ -38,52 +35,34 @@ function check_alias_usage() {
     <"$HISTFILE" | tail "-$limit" | cut -d";" -f2 | while read line; do
         local entry
         for entry in ${(@s/|/)line}; do
-            # Remove leading whitespace
-            # TODO: This is extremely slow
             entry="$(echo "$entry" | sed -e 's/^ *//')"
-
-            # We only care about the first word because that's all aliases work with
-            # (this does not count global and git aliases)
             local word=${entry[(w)1]}
             if [[ -n ${usage[$word]} ]]; then
-                local prev=$usage[$word]
-                let "prev = prev + 1 "
-                usage[$word]=$prev
+                let "usage[$word] = usage[$word] + 1"
             fi
         done
-
-        # print current progress
         let "current = current + 1"
         printf "[$current/$total]\r"
     done
-    # Clear all previous line output
     printf "\r\033[K"
 
-    # Print ordered usage
     for key in ${(k)usage}; do
         echo "${usage[$key]}: $key='${aliases[$key]}'"
     done | sort -rn -k1
 }
 
-# Writing to a buffer rather than directly to stdout/stderr allows us to decide
-# if we want to write the reminder message before or after a command has been executed
 function _write_ysu_buffer() {
     _YSU_BUFFER+="$@"
-
-    # Maintain historical behaviour by default
     local position="${YSU_MESSAGE_POSITION:-before}"
     if [[ "$position" = "before" ]]; then
         _flush_ysu_buffer
     elif [[ "$position" != "after" ]]; then
-        (>&2 printf "${RED}${BOLD}Unknown value for YSU_MESSAGE_POSITION '$position'. ")
-        (>&2 printf "Expected value 'before' or 'after'${NONE}\n")
+        (>&2 printf "${RED}${BOLD}Unknown value for YSU_MESSAGE_POSITION '$position'. Expected value 'before' or 'after'${NONE}\n")
         _flush_ysu_buffer
     fi
 }
 
 function _flush_ysu_buffer() {
-    # It's important to pass $_YSU_BUFFER to printfs first argument
-    # because otherwise all escape codes will not printed correctly
     (>&2 printf "$_YSU_BUFFER")
     _YSU_BUFFER=""
 }
@@ -97,9 +76,6 @@ You should use: ${PURPLE}\"%alias\"${NONE}"
     local command_arg="${2}"
     local alias_arg="${3}"
 
-    # Escape arguments which will be interpreted by printf incorrectly
-    # unfortunately there does not seem to be a nice way to put this into
-    # a function because returning the values requires to be done by printf/echo!!
     command_arg="${command_arg//\%/%%}"
     command_arg="${command_arg//\\/\\\\}"
 
@@ -111,8 +87,6 @@ You should use: ${PURPLE}\"%alias\"${NONE}"
     _write_ysu_buffer "$MESSAGE\n"
 }
 
-
-# Prevent command from running if hardcore mode enabled
 function _check_ysu_hardcore() {
     if [[ "$YSU_HARDCORE" = 1 ]]; then
         _write_ysu_buffer "${BOLD}${RED}You Should Use hardcore mode enabled. Use your aliases!${NONE}\n"
@@ -120,12 +94,10 @@ function _check_ysu_hardcore() {
     fi
 }
 
-
 function _check_git_aliases() {
     local typed="$1"
     local expanded="$2"
 
-    # sudo will use another user's profile and so aliases would not apply
     if [[ "$typed" = "sudo "* ]]; then
         return
     fi
@@ -134,9 +106,6 @@ function _check_git_aliases() {
         local found=false
         git config --get-regexp "^alias\..+$" | sort | while read key value; do
             key="${key#alias.}"
-
-            # if for some reason, read does not split correctly, we
-            # detect that and manually split the key and value
             if [[ -z "$value" ]]; then
                 value="${key#* }"
                 key="${key%% *}"
@@ -154,37 +123,20 @@ function _check_git_aliases() {
     fi
 }
 
-
 function _check_global_aliases() {
     local typed="$1"
     local expanded="$2"
 
     local found=false
-    local tokens
-    local key
-    local value
-    local entry
-
-    # sudo will use another user's profile and so aliases would not apply
-    if [[ "$typed" = "sudo "* ]]; then
-        return
-    fi
-
     alias -g | sort | while read entry; do
-        tokens=("${(@s/=/)entry}")
-        key="${tokens[1]}"
-        # Need to remove leading and trailing ' if they exist
-        value="${(Q)tokens[2]}"
-
-        # Skip ignored global aliases
+        local tokens=("${(@s/=/)entry}")
+        local key="${tokens[1]}"
+        local value="${(Q)tokens[2]}"
         if [[ ${YSU_IGNORED_GLOBAL_ALIASES[(r)$key]} == "$key" ]]; then
             continue
         fi
 
-        if [[ "$typed" = *" $value "* || \
-              "$typed" = *" $value" || \
-              "$typed" = "$value "* || \
-              "$typed" = "$value" ]]; then
+        if [[ "$typed" = *" $value "* || "$typed" = *" $value" || "$typed" = "$value "* || "$typed" = "$value" ]]; then
             ysu_message "global alias" "$value" "$key"
             found=true
         fi
@@ -195,66 +147,46 @@ function _check_global_aliases() {
     fi
 }
 
-
 function _check_aliases() {
     local typed="$1"
     local expanded="$2"
 
-    local found_aliases
-    found_aliases=()
+    local found_aliases=()
     local best_match=""
     local best_match_value=""
     local key
     local value
 
-    # sudo will use another user's profile and so aliases would not apply
     if [[ "$typed" = "sudo "* ]]; then
         return
     fi
 
-    # Find alias matches
     for key in "${(@k)aliases}"; do
         value="${aliases[$key]}"
-
-        # Skip ignored aliases
         if [[ ${YSU_IGNORED_ALIASES[(r)$key]} == "$key" ]]; then
             continue
         fi
 
-        if [[ "$typed" = "$value" || \
-              "$typed" = "$value "* ]]; then
-
-        # if the alias longer or the same length as its command
-        # we assume that it is there to cater for typos.
-        # If not, then the alias would not save any time
-        # for the user and so doesn't hold much value anyway
-        if [[ "${#value}" -gt "${#key}" ]]; then
-
-            found_aliases+="$key"
-
-            # Match aliases to longest portion of command
-            if [[ "${#value}" -gt "${#best_match_value}" ]]; then
-                best_match="$key"
-                best_match_value="$value"
-            # on equal length, choose the shortest alias
-            elif [[ "${#value}" -eq "${#best_match}" && ${#key} -lt "${#best_match}" ]]; then
-                best_match="$key"
-                best_match_value="$value"
+        if [[ "$typed" = "$value" || "$typed" = "$value "* ]]; then
+            if [[ "${#value}" -gt "${#key}" ]]; then
+                found_aliases+="$key"
+                if [[ "${#value}" -gt "${#best_match_value}" ]]; then
+                    best_match="$key"
+                    best_match_value="$value"
+                elif [[ "${#value}" -eq "${#best_match}" && ${#key} -lt "${#best_match}" ]]; then
+                    best_match="$key"
+                    best_match_value="$value"
+                fi
             fi
-        fi
         fi
     done
 
-    # Print result matches based on current mode
     if [[ "$YSU_MODE" = "ALL" ]]; then
         for key in ${(@ok)found_aliases}; do
             value="${aliases[$key]}"
             ysu_message "alias" "$value" "$key"
         done
-
     elif [[ (-z "$YSU_MODE" || "$YSU_MODE" = "BESTMATCH") && -n "$best_match" ]]; then
-        # make sure that the best matched alias has not already
-        # been typed by the user
         value="${aliases[$best_match]}"
         if [[ "$typed" = "$best_match" || "$typed" = "$best_match "* ]]; then
             return
@@ -275,7 +207,7 @@ function disable_you_should_use() {
 }
 
 function enable_you_should_use() {
-    disable_you_should_use   # Delete any possible pre-existing hooks
+    disable_you_should_use
     add-zsh-hook preexec _check_aliases
     add-zsh-hook preexec _check_global_aliases
     add-zsh-hook preexec _check_git_aliases
